b1d5af52836d71c3c582dd5f9d6653d95581c2ac
Samira <tokhirovasamira.17@gmail.com>
2024-08-18 11:56:21 +0500

    test

--- libs/utils.js
+++ libs/utils.js
@@ -10,4 +10,23 @@ function reload(arr, place, Element) {
     }
 }
 
-export{ reload }
+function mainSwiper(){
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+}
+
+export{ reload, mainSwiper }
--- main.js
+++ main.js
@@ -1,45 +1,53 @@
 // import { Genres } from "./components/genres";
 import { makeHeader } from "./components/header";
 import { getData } from "./libs/http";
-import { reload } from "./libs/utils";
-
+import { mainSwiper, reload } from "./libs/utils";
+mainSwiper()
 makeHeader()
 
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
-export function modalOpen () {
-  const modal = document.querySelector('.modal')
-  const modalOpen = document.querySelector('.user-box')
-  const close = document.querySelector('.modal__close')
-  modalOpen.onclick = () => {
-    modal.style.display = 'block'
-    modal.classList.add ('fade')
+const modal = document.querySelector('.modal')
+const modalOpen = document.querySelector('.user-box')
+const close = document.querySelector('.modal__close')
+modalOpen.onclick = () => {
+  modal.style.display = 'block'
+  modal.classList.add ('fade')
   }
  
 close.onclick = () => {
   modal.style.display = 'none'
 }
+
+const form = document.forms[0]
+const inputs = document.querySelectorAll('.required')
+
+form.onsubmit = (e) =>  {
+  e.preventDefault()
+
+  let user = {}
+  let fn = new FormData(form)
+  let token = generateToken();
+  let userId= localStorage.getItem('userId')
+
+  fn.forEach((value, key) => {
+    user[key] = value;
+  })
+
 }
 
-modalOpen()
+function generateToken() {
+  const characters = "ADYUGKGLkhjgguaihfuuoh868658780"
+
+  let res = ""
+  for (let i = 0; i < 10; i++) {
+      let random = Math.floor(Math.random() * characters.length)
+      res += characters[random]
+  }
+  return res;
+}
+
  
-  // getData('goods')
-  // .then(res => {
-  //   console.log(reload(res.data));
-  // })
-  // .catch(errpr => console.error(errpr))
+  getData('goods')
+  .then(res => {
+    console.log(reload(res.data));
+  })
+  .catch(error => console.error(error))
